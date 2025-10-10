490b9c8ff60ab3d03bd64eeebb0021dcadc0725a
joybek-asel <erikbaevaovechka@gmail.com>
2025-10-10 15:39:50 +0500

    Add med-center-service

---
+++ med-center-service/src/test/java/org/example/medcenterservice/MedCenterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.medcenterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedCenterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
