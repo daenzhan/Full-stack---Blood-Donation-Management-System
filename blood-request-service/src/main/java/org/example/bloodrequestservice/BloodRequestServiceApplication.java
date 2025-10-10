82a83e336d5e75042481555d8ea31669d3feda84
joybek-asel <erikbaevaovechka@gmail.com>
2025-10-10 15:44:54 +0500

    Add blood-request-service

---
+++ blood-request-service/src/main/java/org/example/bloodrequestservice/BloodRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.bloodrequestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodRequestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodRequestServiceApplication.class, args);
+    }
+
+}
---
+++ blood-request-service/src/test/java/org/example/bloodrequestservice/BloodRequestServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.bloodrequestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class BloodRequestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
