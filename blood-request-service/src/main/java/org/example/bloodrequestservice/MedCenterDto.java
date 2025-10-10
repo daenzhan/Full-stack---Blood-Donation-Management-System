9af2b57c787b74cf55bb578bc1dc1421213a06c7
joybek-asel <erikbaevaovechka@gmail.com>
2025-10-10 21:32:00 +0500

    Implement in blood-request-service a layer with medcenter integration

--- blood-request-service/src/main/java/org/example/bloodrequestservice/MedCenterDto.java
+++ blood-request-service/src/main/java/org/example/bloodrequestservice/MedCenterDto.java
@@ -1,4 +1,71 @@
 package org.example.bloodrequestservice;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+
 public class MedCenterDto {
+    private Long med_center_id;
+    private String name;
+    private String location;
+    private String phone;
+    private String specialization;
+    private Long user_id;
+    private String license_file;
+
+    public Long getMed_center_id() {
+        return med_center_id;
+    }
+
+    public void setMed_center_id(Long med_center_id) {
+        this.med_center_id = med_center_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getLicense_file() {
+        return license_file;
+    }
+
+    public void setLicense_file(String license_file) {
+        this.license_file = license_file;
+    }
 }
---
+++ blood-request-service/src/main/java/org/example/bloodrequestservice/MedCenterFeignClient.java
@@ -0,0 +1,18 @@
+package org.example.bloodrequestservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "med-center-server")
+public interface MedCenterFeignClient {
+
+    @GetMapping("/medcenters/client/{medcenter_id}")
+    MedCenterDto get_medcenter_by_id(@PathVariable Long medcenter_id);
+
+    @GetMapping("/medcenters/client/search")
+    List<MedCenterDto> search_by_name(@RequestParam String name);
+}
